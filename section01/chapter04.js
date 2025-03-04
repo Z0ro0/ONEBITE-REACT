746e8b8bad63fc93e92f474980f0fdbbb2f38481
Z0ro0 <chlrkdmf0805@gmail.com>
2025-03-04 19:15:48 +0900

    변수와 상수수

--- section01/chapter04.js
+++ section01/chapter04.js
@@ -1,6 +1,9 @@
 // 1. 변수
 let age;
-console.log(age);
-
+console.log(age); //undefined
 age = 30;
 console.log(age);
+
+//2. 상수 -> 변할 수 없기 때문에 반드시 초기화
+const birth = "1997.08.01"
+birth = "123"; // 오류
